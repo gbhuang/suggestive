7687868a439c1cd92fe18d27285de9e49e08a9fe
Gary Huang <garyhuang@Garys-MacBook-Pro.local>
2023-01-06 22:30:25 -0500

    updated function to get book ratings to allow for optional filter for five star reviews only, also necessitated adding check to see if goodreads sign-in banner was active and if so reloadin pae

--- suggestive/suggest_goodreads.py
+++ suggestive/suggest_goodreads.py
@@ -1,15 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-# import requests
 from bs4 import BeautifulSoup
+import time
 
 # TODOS:
-# - switch to selenium to get html
 # - add option to limit to 5 stars
 # - add default limit for max number of reviews
 # - add code to get more than 30 reviews
 
-def get_ratings(book_url):
+def get_ratings(book_url, filter_five=False):
     score_mapping = {'it was amazing': 5,
                      'really liked it': 4,
                      'liked it': 3,
@@ -21,6 +21,22 @@ def get_ratings(book_url):
     ratings = {}
     # response = requests.get(book_url)
     driver.get(book_url)
+
+    if filter_five:
+        clicked = False
+        while not clicked:
+            try:
+                time.sleep(2)
+                driver.find_element(By.LINK_TEXT, 'More filters').click()
+                clicked = True
+            except ElementClickInterceptedException:
+                # sign-in banner blockingg
+                driver.get(book_url)
+
+        time.sleep(2)
+        driver.find_element(By.PARTIAL_LINK_TEXT, '5 stars ').click()
+        time.sleep(2)
+
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     reviews = soup.find_all('div', class_='review')
 
@@ -31,10 +47,10 @@ def get_ratings(book_url):
             'href'].split('/')[-1]
         ratings[user] = score
 
-    driver.quit()
+    # driver.quit()
     return ratings
 
 
 if __name__ == '__main__':
     url = 'https://www.goodreads.com/book/show/6587879-horns'
-    ratings = get_ratings(url)
+    ratings = get_ratings(url, filter_five=True)
