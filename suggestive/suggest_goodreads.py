781e9868c3d43ce8a3fd666d0419a22478c0f230
Gary Huang <garyhuang@Garys-MacBook-Pro.local>
2023-01-07 11:44:17 -0500

    finished initial version of goodreads code to pull down ratings given a book url; added function for suggesting book by projection

--- suggestive/suggest_goodreads.py
+++ suggestive/suggest_goodreads.py
@@ -4,12 +4,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
 
-# TODOS:
-# - add option to limit to 5 stars
-# - add default limit for max number of reviews
-# - add code to get more than 30 reviews
 
-def get_ratings(book_url, filter_five=False):
+def suggest_goodreads_book_by_projection(book_urls):
+    # TODO:
+    # - get 5 star ratings for all book urls
+    # - get intersection of reviewers
+    # - get 5 star ratings for reviewers
+    # - present list sorted by most 5 star ratings
+    pass
+
+
+def _get_ratings(book_url, filter_five=False):
     score_mapping = {'it was amazing': 5,
                      'really liked it': 4,
                      'liked it': 3,
@@ -19,7 +24,6 @@ def get_ratings(book_url, filter_five=False):
     driver = webdriver.Chrome(ChromeDriverManager().install())
 
     ratings = {}
-    # response = requests.get(book_url)
     driver.get(book_url)
 
     if filter_five:
@@ -29,28 +33,37 @@ def get_ratings(book_url, filter_five=False):
                 time.sleep(2)
                 driver.find_element(By.LINK_TEXT, 'More filters').click()
                 clicked = True
-            except ElementClickInterceptedException:
-                # sign-in banner blockingg
+            except:
+                # sign-in banner blocking, or wrong version of site
                 driver.get(book_url)
 
         time.sleep(2)
         driver.find_element(By.PARTIAL_LINK_TEXT, '5 stars ').click()
         time.sleep(2)
 
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    reviews = soup.find_all('div', class_='review')
+    for ii in range(1, 11):
+        if ii > 1:  # advance to next set of reviews
+            try:
+                driver.find_element(By.LINK_TEXT, str(ii)).click()
+                time.sleep(3)
+            except:
+                break
 
-    for review in reviews:
-        score = review.find_all('span', class_='staticStars')
-        score = score_mapping[score[0]['title']]
-        user = review.find_all('a', class_='user')[0][
-            'href'].split('/')[-1]
-        ratings[user] = score
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        reviews = soup.find_all('div', class_='review')
 
+        for review in reviews:
+            score = review.find_all('span', class_='staticStars')
+            score = score_mapping[score[0]['title']]
+            user = review.find_all('a', class_='user')[0][
+                'href'].split('/')[-1]
+            ratings[user] = score
+
+    # TODO: left below commented out for debugging purposes
     # driver.quit()
     return ratings
 
 
 if __name__ == '__main__':
     url = 'https://www.goodreads.com/book/show/6587879-horns'
-    ratings = get_ratings(url, filter_five=True)
+    ratings = _get_ratings(url, filter_five=True)
