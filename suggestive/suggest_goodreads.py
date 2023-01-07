9300209128e19627a0859fedcac54df59cd1790f
Gary Huang <garyhuang@Garys-MacBook-Pro.local>
2023-01-07 14:51:07 -0500

    added caching of book ratings, looping over books for suggest by projection function

--- suggestive/suggest_goodreads.py
+++ suggestive/suggest_goodreads.py
@@ -3,18 +3,42 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
+import os
+import pickle
 
 
 def suggest_goodreads_book_by_projection(book_urls):
     # TODO:
-    # - get 5 star ratings for all book urls
-    # - get intersection of reviewers
     # - get 5 star ratings for reviewers
     # - present list sorted by most 5 star ratings
-    pass
+
+    all_ratings = []
+    for book_url in book_urls:
+        all_ratings.append(_get_ratings(book_url, filter_five=True))
+
+    common_reviewers = all_ratings[0].keys()
+    print('initial num reviewers: {}'.format(len(common_reviewers)))
+
+    for ii in range(1, len(all_ratings)):
+        common_reviewers = common_reviewers & all_ratings[ii].keys()
+        print('n_books = {}, num reviewers: {}'.format(
+            ii, len(common_reviewers)))
+
+    return common_reviewers
 
 
 def _get_ratings(book_url, filter_five=False):
+    book_id = book_url.split('/')[-1]
+    book_fn = book_id
+    if filter_five:
+        book_fn += 'filtered'
+    book_fn = 'cached/{}.p'.format(book_fn)
+    if os.path.isfile(book_fn):
+        print('reading result from cache')
+        with open(book_fn, 'rb') as f_in:
+            ratings = pickle.load(f_in)
+        return ratings
+
     score_mapping = {'it was amazing': 5,
                      'really liked it': 4,
                      'liked it': 3,
@@ -61,9 +85,16 @@ def _get_ratings(book_url, filter_five=False):
 
     # TODO: left below commented out for debugging purposes
     # driver.quit()
+
+    if not os.path.isdir('cached'):
+        os.mkdir('cached')
+    with open(book_fn, 'wb') as f_out:
+        pickle.dump(ratings, f_out)
+
     return ratings
 
 
 if __name__ == '__main__':
-    url = 'https://www.goodreads.com/book/show/6587879-horns'
-    ratings = _get_ratings(url, filter_five=True)
+    book_urls = ['https://www.goodreads.com/book/show/6587879-horns',
+                 'https://www.goodreads.com/book/show/23168277-the-sympathizer']
+    oo = suggest_goodreads_book_by_projection(book_urls)
